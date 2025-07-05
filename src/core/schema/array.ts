59778085eb5eb0e65ea245199d0b342ba7f2faa4
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-05 18:50:45 +0700

    refactor: array handlers with union

--- src/core/schema/array.ts
+++ src/core/schema/array.ts
@@ -1,5 +1,6 @@
 import type { Definition } from "~/types/definition";
 import { generateZodSchema } from "../generateZodSchema";
+import { handleZodUnion } from "./union";
 import type { SchemaObject } from "@omer-x/openapi-types/schema";
 
 export function handleZodArray(jsonSchema: SchemaObject): Definition {
@@ -7,14 +8,8 @@ export function handleZodArray(jsonSchema: SchemaObject): Definition {
     throw new Error("Invalid schema type for Zod array handler");
   }
   if (Array.isArray(jsonSchema.items)) {
-    const dependencies: string[] = [];
-    const items: string[] = [];
-    for (const item of jsonSchema.items) {
-      const result = generateZodSchema(item);
-      dependencies.push(...result.dependencies);
-      items.push(result.body);
-    }
-    return { dependencies, body: `z.array(z.union([${items.join(", ")}]))` };
+    const union = handleZodUnion(jsonSchema.items);
+    return { dependencies: union.dependencies, body: `z.array(${union.body})` };
   }
   const result = generateZodSchema(jsonSchema.items);
   return { dependencies: result.dependencies, body: `z.array(${result.body})` };
--- src/core/type/array.ts
+++ src/core/type/array.ts
@@ -1,5 +1,6 @@
 import type { Definition } from "~/types/definition";
 import { generateZodType } from "../generateZodType";
+import { handleZodUnion } from "./union";
 import type { SchemaObject } from "@omer-x/openapi-types/schema";
 
 export function handleZodArray(jsonSchema: SchemaObject): Definition {
@@ -7,14 +8,8 @@ export function handleZodArray(jsonSchema: SchemaObject): Definition {
     throw new Error("Invalid schema type for Zod array handler");
   }
   if (Array.isArray(jsonSchema.items)) {
-    const dependencies: string[] = [];
-    const items: string[] = [];
-    for (const item of jsonSchema.items) {
-      const result = generateZodType(item);
-      dependencies.push(...result.dependencies);
-      items.push(result.body);
-    }
-    return { dependencies, body: `z.ZodArray<z.ZodUnion<readonly [${items.join(", ")}]>>` };
+    const union = handleZodUnion(jsonSchema.items);
+    return { dependencies: union.dependencies, body: `z.ZodArray<${union.body}>` };
   }
   const result = generateZodType(jsonSchema.items);
   return { dependencies: result.dependencies, body: `z.ZodArray<${result.body}>` };
